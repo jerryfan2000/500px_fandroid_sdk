f8cdf59adc3943c841df7b0edb2316677910ef3f
jerryfan2000 <kiumiu.ca@gmail.com>
2014-03-29 18:07:12 +0800

    update javadoc

---
+++ src/com/kiumiu/ca/api500px/response/photo/get_photos_id_response.java
@@ -0,0 +1,10 @@
+package com.kiumiu.ca.api500px.response.photo;
+
+import com.kiumiu.ca.api500px.primitiveDataType.Comment;
+import com.kiumiu.ca.api500px.primitiveDataType.PhotoFull;
+
+public class get_photos_id_response {
+	public Comment[] comments;
+	public PhotoFull photo;
+	
+}

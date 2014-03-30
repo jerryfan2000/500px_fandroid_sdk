beaeb39a7ecc9c6254834835a53f51fe71c6ed2d
jerryfan2000 <kiumiu.ca@gmail.com>
2014-03-31 01:01:44 +0800

    added get_users_response class

---
+++ src/com/kiumiu/ca/api500px/response/user/get_users_response.java
@@ -0,0 +1,21 @@
+package com.kiumiu.ca.api500px.response.user;
+
+import com.kiumiu.ca.api500px.primitiveDataType.UserFullProfile;
+
+public class get_users_response {
+	public UserFullProfile user;
+
+	/**
+	 * @return the user
+	 */
+	public UserFullProfile getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(UserFullProfile user) {
+		this.user = user;
+	}
+}
